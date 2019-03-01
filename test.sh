9dc860a4011f4cf23357b33b000340775f15a716
Baoquan Zhang <zhangbaoquan89@gmail.com>
2019-03-01 13:48:09 -0600

    bf

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-PMEM_IS_PMEM_FORCE=1 ./bin/pmemkv_bench --histogram=1 --db=/mnt/pmem/nvlsm/pmemkv.pool --engine=nvlsm2 --value_size=128 --db_size_in_gb=40 --benchmarks=fillrandom,readrandom --num=50000000
+PMEM_IS_PMEM_FORCE=1 ./bin/pmemkv_bench --histogram=1 --db=/mnt/pmem/nvlsm/pmemkv.pool --engine=nvlsm2 --value_size=128 --db_size_in_gb=10 --benchmarks=fillrandom,readrandom --num=1000000
